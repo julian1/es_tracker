6374190784ed7b328ee781d5d50a853459d74fe1
julian1 <julian.atkinson71@gmail.com>
2014-09-20 12:13:57 +1000

    update

--- ggg.rb
+++ ggg.rb
@@ -15,7 +15,8 @@ conn = PG::Connection.open(:dbname => 'test', :user => 'meteo', :password => 'me
 # res = conn.exec_params( 'select id, t, msg, data from queue where msg = $$order$$ order by id limit 1', [] )
 res = conn.exec_params( 'select id, t, msg, data from queue order by id ', [] )
 
-def dostuff( msg, t, data )
+dostuff = Proc.new do | msg, t, data | 
+#def dostuff( msg, t, data )
 	
 	if msg == 'order'
 		#puts "#{msg } #{t}"
@@ -27,7 +28,8 @@ def dostuff( msg, t, data )
 	end
 end
 
-
+# we have to pass a function or block 
+# def processqueue(
 res.each do |row|
 	begin
 		id = row['id'].to_i
@@ -38,17 +40,9 @@ res.each do |row|
 		puts "Exception #{$!}"
 	end
 
-	# ok, we haven't got json - we've just got a string. 
-	# it's not being parsed correctly to_json
 
-	# puts id
-
-	#puts "data type #{data.class}" 
-	#puts " #{data['timestamp']}  " 
-	#puts data
-
-	dostuff(msg, t, data)
+	dostuff.call(msg, t, data)
 end
-#
 
+#
 #uts "ratio #{(bids_sum/asks_sum*100.to_i) / 100 }"
