fefc02679404afb2064767c305b38e9bacbef0cc
julian1 <julian.atkinson71@gmail.com>
2014-09-20 12:49:52 +1000

    update

--- ggg.rb
+++ ggg.rb
@@ -12,7 +12,6 @@ def process_queue( f )
 	res = conn.exec_params( 'select id, t, msg, data from queue order by id ', [] )
 
 	# we have to pass a function or block 
-	# def processqueue(
 	res.each do |row|
 		begin
 			id = row['id'].to_i
@@ -39,6 +38,9 @@ end
 
 
 do_stuff = Proc.new do | msg, t, data | 
+
+  # it would be very interesting to compute a series - based on the number of entries in
+  # the order book within 10% of the price.
 	
 	if msg == 'order'
 		#puts "#{msg } #{t}"
@@ -48,7 +50,6 @@ do_stuff = Proc.new do | msg, t, data |
 
 		bids_sum = compute_sum(data['bids']) 
 		asks_sum = compute_sum(data['asks']) 
-#		puts "sum #{bids_sum}"	
 
 		puts "#{t} total bids:#{bids} asks:#{asks} ratio:#{ratio}  bids_sum:#{bids_sum} asks_sum:#{asks_sum}"
 	end
