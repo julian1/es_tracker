ee5ea564257ff4e2e0fc692882fb14772121b094
julian1 <julian.atkinson71@gmail.com>
2014-09-20 12:28:17 +1000

    update

--- ggg.rb
+++ ggg.rb
@@ -4,19 +4,41 @@ require 'pg'
 require 'date'
 
 
-# s = '{"timestamp": "1411168092", "bids": [["395.24", "2.25000000"]]}'
-# j = JSON.parse( s )
-# puts "#{j['bids']}" 
-# puts "data type #{j.class}" 
-# abort()
 
-conn = PG::Connection.open(:dbname => 'test', :user => 'meteo', :password => 'meteo' )
+def process_queue( f )
+	conn = PG::Connection.open(:dbname => 'test', :user => 'meteo', :password => 'meteo' )
 
-# res = conn.exec_params( 'select id, t, msg, data from queue where msg = $$order$$ order by id limit 1', [] )
-res = conn.exec_params( 'select id, t, msg, data from queue order by id ', [] )
+	# res = conn.exec_params( 'select id, t, msg, data from queue where msg = $$order$$ order by id limit 1', [] )
+	res = conn.exec_params( 'select id, t, msg, data from queue order by id ', [] )
 
-dostuff = Proc.new do | msg, t, data | 
-#def dostuff( msg, t, data )
+	# we have to pass a function or block 
+	# def processqueue(
+	res.each do |row|
+		begin
+			id = row['id'].to_i
+			t = DateTime.parse( row['t'] ) 
+			msg = row['msg']
+			data = JSON.parse( row['data'] )
+			f.call(msg, t, data)
+		rescue
+			puts "Exception #{$!}"
+		end
+	end
+end
+
+
+def compute_sum(data) 
+  bids_sum = 0
+  data.each do |i|
+    price = i[0].to_f
+    quantity = i[1].to_f
+    bids_sum += price * quantity
+  end
+  bids_sum.to_i
+end
+
+
+do_stuff = Proc.new do | msg, t, data | 
 	
 	if msg == 'order'
 		#puts "#{msg } #{t}"
@@ -24,25 +46,15 @@ dostuff = Proc.new do | msg, t, data |
 		asks = data['asks'].length
 		ratio = (bids.to_f / asks.to_f ).round(3) 
 
-		puts "#{t} total bids:#{bids} asks:#{asks} ratio:#{ratio}"
+		bids_sum = compute_sum(data['bids']) 
+		asks_sum = compute_sum(data['asks']) 
+#		puts "sum #{bids_sum}"	
+
+		puts "#{t} total bids:#{bids} asks:#{asks} ratio:#{ratio}  bids_sum:#{bids_sum} asks_sum:#{asks_sum}"
 	end
 end
 
-# we have to pass a function or block 
-# def processqueue(
-res.each do |row|
-	begin
-		id = row['id'].to_i
-		t = DateTime.parse( row['t'] ) 
-		msg = row['msg']
-		data = JSON.parse( row['data'] )
-	rescue
-		puts "Exception #{$!}"
-	end
+
+process_queue( do_stuff)
 
 
-	dostuff.call(msg, t, data)
-end
-
-#
-#uts "ratio #{(bids_sum/asks_sum*100.to_i) / 100 }"
