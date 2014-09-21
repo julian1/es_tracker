d5e8ccb9763d0c99dc30d5dd6110235f9b6f3358
julian1 <julian.atkinson71@gmail.com>
2014-09-21 12:21:24 +1000

    reorganize and preliminary charting prototype

--- calc_series.rb
+++ calc_series.rb
@@ -15,6 +15,7 @@ require 'date'
 
 def process_resultset( res, f )
   # process a result set with events - we have to pass a function or block 
+  # does this stream, from the postgres ? 
   id = -1
   res.each do |row|
     begin
@@ -58,36 +59,36 @@ def process_current_events( conn, last_id, f )
 end
 
 
-def compute_sum(data) 
-  bids_sum = 0
-  data.each do |i|
-    price = i[0].to_f
-    quantity = i[1].to_f
-    bids_sum += price * quantity
-  end
-  bids_sum.to_i
-end
 
 
-# do_stuff = Proc.new do | msg, t, data | 
+# process_event = Proc.new do | msg, t, data | 
 # end 
  
 
-class MyClass
-  def initialize
-    @m = []
+class EventProcessor
+  def initialize()
+    @model = []
   end
 
-  def do_stuff( id, msg, t, data)
+  def compute_sum(data) 
+    bids_sum = 0
+    data.each do |i|
+      price = i[0].to_f
+      quantity = i[1].to_f
+      bids_sum += price * quantity
+    end
+    bids_sum.to_i
+  end
 
-      puts "processing event #{id}"
-
+  # process an event 
+  def process_event( id, msg, t, data)
+     # puts "processing event #{id}"
     if msg == 'order'
-#       # puts "#{time} total bids:#{bids} asks:#{asks} ratio:#{ratio}  bids_sum:#{bids_sum} asks_sum:#{asks_sum}"
-#       #  @m << { :time => time, :bids => bids, :asks => asks } 
+      # puts "#{time} total bids:#{bids} asks:#{asks} ratio:#{ratio}  bids_sum:#{bids_sum} asks_sum:#{asks_sum}"
 
       time = Time.at(data['timestamp'].to_i).to_datetime
       top_bid = data['bids'][0][0]
+      top_ask = data['asks'][0][0]
       bids = data['bids'].length
       asks = data['asks'].length
       ratio = (bids.to_f / asks.to_f ).round(3) 
@@ -95,11 +96,22 @@ class MyClass
       asks_sum = compute_sum(data['asks']) 
       sum_ratio = (bids_sum.to_f / asks_sum.to_f).round(3) 
 
-      #puts "#{t} top_bid #{top_bid} total bids:#{bids} asks:#{asks} ratio:#{ratio}  bids_sum:#{bids_sum} asks_sum:#{asks_sum} ratio:#{sum_ratio}"
-  # 
-      puts "#{time}, #{top_bid}, #{sum_ratio}"
-
+      # puts "#{t} top_bid #{top_bid} total bids:#{bids} asks:#{asks} ratio:#{ratio}  bids_sum:#{bids_sum} asks_sum:#{asks_sum} ratio:#{sum_ratio}"
+  #
+      # puts "#{time}, #{top_ask}, #{sum_ratio * 1700}"
+      @model << [ id, top_ask ] 
     end
+  end
+
+  # access the current model state  
+  # this can be performed at any time ...
+  def get()
+    # to be fast, we should really use a stream 
+    # should be a join, 
+    l = @model.map do |row|
+      "[ #{row[0]}, #{row[1]} ]" 
+    end
+    "[#{l.join(", ")}]"
   end
 
 end
@@ -109,18 +121,16 @@ end
 
 conn = PG::Connection.open(:dbname => 'test', :user => 'meteo', :password => 'meteo' )
 
-x = MyClass.new()
-f = proc { |a,b,c,d| x.do_stuff(a,b,c,d) }
-
+x = EventProcessor.new()
+f = proc { |a,b,c,d| x.process_event(a,b,c,d) }
 
 
 last = process_historic_events( conn, f )
 
-#puts "last id #{last}"
-#last = 940
-process_current_events( conn, last , f )
+#process_current_events( conn, last , f )
 
 
+puts x.get()
 
 
 # need json keys to array - for the show messages
