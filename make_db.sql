327a20a94ddc3a463ab87e856e33e3b5010db8ca
julian1 <julian.atkinson71@gmail.com>
2014-10-01 21:41:37 +1000

    update

--- make_db.sql
+++ make_db.sql
@@ -3,21 +3,36 @@
 
 -- echo 'localhost:5432:*:meteo:meteo' > ~/.pgpass
 
-drop database if exists test; 
-drop user if exists meteo ; 
-create user meteo password 'meteo'; 
+-- expects to have the user meteo
 
-create database test; 
+create database test2; 
 
-\c test; 
+\c test2; 
 
 
 create schema meteo authorization meteo;
 
--- need to change name to events
--- change name to events
 CREATE TABLE events ( id serial primary key, t timestamptz, msg varchar(10), data json ); 
 alter table events owner to meteo;
+
+
+
+CREATE or replace PROCEDURAL LANGUAGE plpgsql;
+
+-- Create a trigger function in PL/pgSQL.
+
+CREATE FUNCTION notify_trigger() RETURNS trigger AS $$
+DECLARE
+BEGIN
+ -- TG_TABLE_NAME is the name of the table who's trigger called this function
+ -- TG_OP is the operation that triggered this function: INSERT, UPDATE or DELETE.
+ execute 'NOTIFY ' || TG_TABLE_NAME || '_' || TG_OP;
+ return new;
+END;
+$$ LANGUAGE plpgsql;
+-- Create triggers on the test tables
+
+CREATE TRIGGER table1_trigger BEFORE insert or update or delete on events execute procedure notify_trigger();
 
 
 
