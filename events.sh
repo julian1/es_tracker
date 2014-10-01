dc13a35c3c5bd4c699ec2b0c6078f22b5ef1f5dd
julian1 <julian.atkinson71@gmail.com>
2014-10-02 07:27:46 +1000

    add events orders errors

--- events.sh
+++ events.sh
@@ -1,4 +1,4 @@
 
 
-psql -d test2 -c 'select e.id, to_char( e.t, $$YYYYMMDD HH24:MI:SS$$ ), e.msg from events e '
+psql -d test2 -c 'select e.id, to_char( e.t, $$YYYYMMDD HH24:MI:SS$$ ), e.msg from events e order by id'
 
