f82f95f583e068899c62d80d0dd150e05de6f26b
julian1 <julian.atkinson71@gmail.com>
2014-10-02 08:28:02 +1000

    tidy

--- ingest.rb
+++ ingest.rb
@@ -1,8 +1,5 @@
 
-# A service to populate the db, with bitstamp data.
-# Do not make more than 600 request per 10 minutes or we will ban your IP address.
-# https://www.bitstamp.net/api/
-
+# A service to populate the event queue, with data.
 
 require 'json'
 require 'open-uri'
@@ -54,19 +51,19 @@ class MyClient
       end
         # must be sleep when get exception too
 
-      # try and schedule according to interval
+      # try and schedule actions on a multiple of the timing interval
       begin
         start_time = start_time + interval
         sleep_time = (start_time - Time.now ).to_i
       end while sleep_time < 0
 
-      puts "sleep #{url} #{sleep_time}"
+      # puts "sleep #{url} #{sleep_time}"
       sleep( sleep_time  )
     end 
     }
   end
 
-  def wait()
+  def run()
     @threads.each() do |t|
       t.join()
     end
@@ -78,7 +75,6 @@ end
 
 db_params = { :dbname => 'test2', :user => 'meteo', :password => 'meteo' }
 
-# res = conn.exec_params('SELECT $1::int AS a, $2::int AS b, $3::int AS c', [1, 2, nil])
 
 client = MyClient.new( db_params )
 
@@ -87,44 +83,6 @@ client.start_client( 'https://www.bitstamp.net/api/ticker/', 60)
 client.start_client( 'https://api.btcmarkets.net/market/BTC/AUD/orderbook', 60)
 client.start_client( 'https://api.btcmarkets.net/market/BTC/AUD/trades', 3 * 60)
 
-client.wait()
-
-  # 
-  # i = 0
-  # while true do
-  #   begin
-  #     # puts i 
-  #     open("https://www.bitstamp.net/api/order_book/", "rb") do |read_file|
-  #       res = conn.exec_params( 'insert into events( t, msg, data) values( now()::timestamptz, $$order$$, $1::json )', 
-  #         [read_file.read] )
-  #     end
-  # 
-  #     open("https://www.bitstamp.net/api/ticker/", "rb") do |read_file|
-  #       res = conn.exec_params( 'insert into events( t, msg, data) values( now()::timestamptz, $$ticker$$, $1::json )', 
-  #         [read_file.read] )
-  #     end
-  #   rescue
-  #     begin
-  #       # On error just record the error
-  #       res = conn.exec_params( 'insert into events( t, msg, data) values( now()::timestamptz, $$error$$, $1::json )', 
-  #           [$!.to_json] )
-  #     rescue
-  #       # if cant write db, then there's nothing else we can do, so write std-out
-  #       puts "#{Time.now()}: exception error - #{$!}"
-  #     end
-  #   end
-  # 
-  #   if i == 0
-  #     puts "started successfully."
-  #   end
-  # 
-  #   i = i + 1
-  #   sleep(60.0)
-  # end
-
-  # res.each do |row|
-  #   puts row
-  # end
-  # 
+client.run()
 
 
