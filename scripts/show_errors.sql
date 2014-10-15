30d4b50690dcec7ca16b3d1a553f713949147283
julian1 <julian.atkinson71@gmail.com>
2014-10-15 21:37:29 +1100

    show errors dont limit data

--- scripts/show_errors.sql
+++ scripts/show_errors.sql
@@ -6,7 +6,8 @@ select
 	to_char( e.t, $$YYYY-MM-DD HH24:MI:SS$$ ) as t, 
 	e.origin as origin,
 	e.msg, 
-	left( e.content ::varchar, 60)
+	--left( e.content ::varchar, 60)
+	e.content ::varchar
 from events e 
 where e.msg = $$error$$ 
 order by id desc
